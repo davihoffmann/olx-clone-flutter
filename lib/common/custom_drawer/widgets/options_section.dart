dd43e848659aa09bde0d7240056b474aff369026
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-07-21 11:42:25 -0300

    criada nevagaçao atraves de drawer, controlado por bloc pattern

--- lib/common/custom_drawer/widgets/options_section.dart
+++ lib/common/custom_drawer/widgets/options_section.dart
@@ -1,44 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xlo/blocs/drawer_bloc.dart';
 import 'package:xlo/common/custom_drawer/widgets/option_tile.dart';
 
 class OptionsSection extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        OptionTile(
-          label: 'Anúncios',
-          iconData: Icons.list,
-          onTap: () => {},
-          highlighted: false,
-        ),
-        OptionTile(
-          label: 'Inserir Anúncio',
-          iconData: Icons.edit,
-          onTap: () => {},
-          highlighted: false,
-        ),
-        OptionTile(
-          label: 'Chat',
-          iconData: Icons.chat,
-          onTap: () => {},
-          highlighted: false,
-        ),
-        OptionTile(
-          label: 'Favoritos',
-          iconData: Icons.favorite,
-          onTap: () => {},
-          highlighted: false,
-        ),
-        OptionTile(
-          label: 'Minha Conta',
-          iconData: Icons.person,
-          onTap: () => {},
-          highlighted: false,
-        )
-      ],
+    final DrawerBloc _drawerBloc = Provider.of<DrawerBloc>(context);
+
+    void _setPage(int page) {
+      Navigator.of(context).pop();
+      _drawerBloc.setPage(page);
+    }
+
+    return StreamBuilder(
+      stream: _drawerBloc.outPage,
+      builder: (context, snapshot) {
+        return Column(
+          children: <Widget>[
+            OptionTile(
+              label: 'Anúncios',
+              iconData: Icons.list,
+              onTap: () => _setPage(0),
+              highlighted: snapshot.data == 0,
+            ),
+            OptionTile(
+              label: 'Inserir Anúncio',
+              iconData: Icons.edit,
+              onTap: () => _setPage(1),
+              highlighted: snapshot.data == 1,
+            ),
+            OptionTile(
+              label: 'Chat',
+              iconData: Icons.chat,
+              onTap: () => _setPage(2),
+              highlighted: snapshot.data == 2,
+            ),
+            OptionTile(
+              label: 'Favoritos',
+              iconData: Icons.favorite,
+              onTap: () => _setPage(3),
+              highlighted: snapshot.data == 3,
+            ),
+            OptionTile(
+              label: 'Minha Conta',
+              iconData: Icons.person,
+              onTap: () => _setPage(4),
+              highlighted: snapshot.data == 4,
+            )
+          ],
+        );
+      },
     );
   }
-
 }
